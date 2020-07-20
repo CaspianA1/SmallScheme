20ffe23c62964a83a1a33bddf24bf843a2c12b81
Your Name <you@example.com>
2020-07-19 23:36:05 -0400

    Make tli even smaller

--- tli.py
+++ tli.py
@@ -1,15 +1,14 @@
-import operator as oper
-from functools import reduce
+import operator as oper; from functools import reduce
+
 num_op = lambda op, nums: reduce(lambda x, y: op(x, y), nums)
-std_lib = {
+env = {
         "+": lambda *nums: sum(nums),
         "-": lambda *nums: num_op(oper.sub, nums),
         "*": lambda *nums: num_op(oper.mul, nums),
         "/": lambda *nums: num_op(oper.truediv, nums),
-        "define": lambda name, val: symbol_table.update({name: val}),
+        "define": lambda name, val: env.update({name: val}),
         "display": lambda var: print(find_val(var))
-    }
-symbol_table = {}
+}
 def parse(tokens):
     if not tokens: return
     curr = tokens.pop(0)
@@ -26,10 +25,8 @@ def parse(tokens):
             try: return float(curr)
             except ValueError: return str(curr)
 def find_val(var):
-    if var in symbol_table: return symbol_table[var]
-    elif var in std_lib: return std_lib[var]
+    if var in env: return env[var]
     else: return var
 eval_ = lambda code: find_val(code[0])(*[eval_(arg) for arg in code[1:]]) if isinstance(code, list) else find_val(code)
 if __name__ == "__main__":
-    while True:
-        eval_(parse(input("> ").replace("(", " ( ").replace(")", " ) ").split()))
+    while True: eval_(parse(input("> ").replace("(", " ( ").replace(")", " ) ").split()))
