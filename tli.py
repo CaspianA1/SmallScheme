c8b134c2956646ecab15e89940caa79a32c36a0f
Your Name <you@example.com>
2020-07-20 17:48:08 -0400

    Configure tli just a bit more

--- tli.py
+++ tli.py
@@ -1,14 +1,11 @@
 import operator as oper; from functools import reduce
 
 num_op = lambda op, nums: reduce(lambda x, y: op(x, y), nums)
-env = {
-        "+": lambda *nums: sum(nums),
-        "-": lambda *nums: num_op(oper.sub, nums),
-        "*": lambda *nums: num_op(oper.mul, nums),
-        "/": lambda *nums: num_op(oper.truediv, nums),
-        "define": lambda name, val: env.update({name: val}),
-        "display": lambda var: print(find_val(var))
-}
+
+env = {"+": lambda *nums: sum(nums), "-": lambda *nums: num_op(oper.sub, nums), "*": lambda *nums: num_op(oper.mul, nums),
+       "/": lambda *nums: num_op(oper.truediv, nums), "=": lambda *nums: num_op(oper.eq, nums),">": lambda n1, n2: n1 > n2,
+       "<": lambda n1, n2: n1 < n2, "define": lambda name, val: env.update({name: val}), "display": lambda var: print(find_val(var))}
+
 def parse(tokens):
     if not tokens: return
     curr = tokens.pop(0)
@@ -24,9 +21,8 @@ def parse(tokens):
         except ValueError:
             try: return float(curr)
             except ValueError: return str(curr)
-def find_val(var):
-    if var in env: return env[var]
-    else: return var
+
+find_val = lambda var: env[var] if var in env else var
 eval_ = lambda code: find_val(code[0])(*[eval_(arg) for arg in code[1:]]) if isinstance(code, list) else find_val(code)
-if __name__ == "__main__":
-    while True: eval_(parse(input("> ").replace("(", " ( ").replace(")", " ) ").split()))
+
+while True: eval_(parse(input("> ").replace("(", " ( ").replace(")", " ) ").split()))
